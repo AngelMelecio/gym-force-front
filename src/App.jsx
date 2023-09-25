10605c6e2d0732a422d104f1ec77c6d298196cee
AngelMelecio <joseangelsm50@gmail.com>
2023-09-24 18:10:26 -0600

    JWT Implementation (signIn, signOut authContext)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Grid from './components/Grid'
+import AppBar from './components/navigation/AppBar'
+import AppRouter from './components/navigation/AppRouter'
+import { useAuth } from './context/authContext'
+import LoginPage from './pages/LoginPage'
 function App() {
 
+  const { session } = useAuth()
 
   return (
-    <>
-      {/* Cuadricula pal layout 
-      */}
-      <Grid/>
-
-    </>
+    session ? (
+      <div className='flex flex-row'>
+        <AppBar />
+        <AppRouter />
+      </div>
+    ) : (
+      <Routes>
+        <Route exact path="*" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    )
   )
 }
 
